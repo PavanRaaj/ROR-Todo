9b83c0f993776fa59a10fb560bd9202b410d6be2
PavanRaaj <pavanraajgoud@gmail.com>
2022-06-28 19:21:11 +0530

    Added files

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
  
   # get 'users/register'
 
-  # get 'todos/todo_app'
+  get 'todos#todo_app'
 
    match 'todos/todo_app', :to => 'todos#todo_app', :via => :get
    match 'todos/create', :to => 'todos#create', :via => :post
